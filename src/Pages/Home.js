6b0b47abf9e9c15fe1dd130ed2faee853aa3b954
Harshit Swarnkar <96280175+Rew0611@users.noreply.github.com>
2022-08-21 21:39:50 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -112,7 +112,7 @@ const Home = () => {
                     <div className="service-details" data-wow-delay=".1s">
                       <div className="service-head">
                         <img
-                          src="public/assets/img/service/a.jpg"
+                          src="assets/img/service/a.jpg"
                           alt="design-development"
                         />
                       </div>
